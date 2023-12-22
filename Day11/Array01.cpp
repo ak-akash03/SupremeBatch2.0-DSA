83c5d24b405b8369991cda58038df0b5d4533d0b
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-22 14:34:02 +0530

    ....

--- Day11/Array01.cpp
+++ Day11/Array01.cpp
@@ -11,21 +11,38 @@ using namespace std;
     
 // }
 
-void printPairs(int arr[], int size){
+// void printPairs(int arr[], int size){
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout<<"("<<arr[i]<<","<<arr[j]<<")  ";
-        }
-        cout<<endl;
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = 0; j < size; j++)
+//         {
+//             cout<<"("<<arr[i]<<","<<arr[j]<<")  ";
+//         }
+//         cout<<endl;
         
-    }
-    
+//     }
+
+// }
 
 
-}
+// void printPairs(int arr[], int size){
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = 0; j < size; j++)
+//         {
+//             for (int k = 0; k < size; k++)
+//             {
+//                 cout<<"("<<arr[i]<<","<<arr[j]<<","<<arr[k]<<")  ";
+//             }
+//             cout<<endl;
+//         }
+//         cout<<endl;       
+        
+//     }
+
+// }
 
 int main(){
     // int arr[] = {2,10,11,10,2,13,15,13,15};
@@ -36,17 +53,8 @@ int main(){
 
 
 
-    int arr[] = {1,2,3};
-    int size = 3;
-    printPairs(arr,size);
-
-
-
-
-
-
-
-
-
+    // int arr[] = {1,2,3,4,5};
+    // int size = 3;
+    // printPairs(arr,size);
 
 } 
