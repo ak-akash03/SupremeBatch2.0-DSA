0b4b31c06bdebba8a4c20973258e9b2d661b4e45
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-22 11:57:35 +0530

    commit on date 22-12-2023

---
+++ Day09/arr001.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int arr[5];
+    // int n = 5;
+    // cout<<"enter the input : ";
+    // for (int  i = 0; i < n; i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // int sum = 0;
+    // cout<<endl<<"addition : "<<endl;
+    // for (int j = 0; j < n; j++)
+    // {
+    //     sum = sum + arr[j];
+    // }
+    // cout<<sum;
+    
+// *************************************************************
+
+    // int arr[] = {2,4,7,9,1,3,5};
+    // cout<<"enter the input : "; 
+    // int find ;
+    // cin>>find;
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     if(find == arr[i]){
+    //         cout<<"index : "<<i<<endl;
+    //         break;
+    //     }
+    // }
+    
+
+// **********************************************************************
+
+    // int arr[5] = {6,3,7,2,5};
+    // int target;
+    // cout<<"enter the target : ";
+    // cin>>target;
+
+    // bool out = 0;
+
+    // for (int  i = 0; i < 5; i++)
+    // {
+    //     if(target == arr[i]){
+    //         out = 1;
+    //     }
+    // }
+    // if(out == 1){
+    //     cout<<"target is found :";
+    // }else{
+    //     cout<<"target is not found : ";
+    // }
+
+
+}
