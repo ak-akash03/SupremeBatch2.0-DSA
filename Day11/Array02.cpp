05bed8232f09b7c50b5f2168890ef507df64bb70
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-22 14:43:51 +0530

    push Array02.cpp

---
+++ Day11/Array02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {0,0,1,1,0,0,0,0,1};
+    int size = 9;
+    
+    // for (int i = 0; i < size; i++)
+    // {
+    //     for (int j = 0; j < size; j++)
+    //     {
+    //         if(arr[i]<arr[j]){
+    //             swap(arr[i],arr[j]);
+    //         }
+    //     }
+        
+    // }
+
+    // for (int k = 0; k < size; k++)
+    // {
+    //     cout<<arr[k]<<" ";
+    // }
+    
+    
+   
+    
+
+}
