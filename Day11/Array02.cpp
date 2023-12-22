5ed0ca456af6f3ec74ed18edc0dcb35be791389d
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-22 14:57:22 +0530

    push Array03.cpp:

--- Day11/Array02.cpp
+++ Day11/Array02.cpp
@@ -1,25 +1,74 @@
 #include <iostream>
 using namespace std;
+
+// void countZeroOne(int arr[], int size){
+
+//     int countZero = 0;
+//     int countOne = 0;
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         if(arr[i] == 0){
+//             countZero++;
+//         }
+//         if(arr[i] == 1){
+//             countOne++;
+//         }
+//     }
+
+//     int k = 0;
+//     for (; k < countZero; k++)
+//     {
+//         arr[k] = 0;
+//     }
+//     for (int m = k; m < size; m++)
+//     {
+//         arr[m] = 1;
+//     }
+    
+
+//     // print
+
+//     for (int l = 0; l < size; l++)
+//     {
+//         cout<<arr[l]<<" ";
+//     }
+    
+    
+    
+
+// }
+
 int main()
 {
-    int arr[] = {0,0,1,1,0,0,0,0,1};
-    int size = 9;
-    
-    // for (int i = 0; i < size; i++)
-    // {
-    //     for (int j = 0; j < size; j++)
-    //     {
-    //         if(arr[i]<arr[j]){
-    //             swap(arr[i],arr[j]);
-    //         }
-    //     }
-        
-    // }
+    // int arr[] = {0,0,1,1,0,0,0,0,1};
+    // int size = 9;
 
-    // for (int k = 0; k < size; k++)
-    // {
-    //     cout<<arr[k]<<" ";
-    // }
+    // countZeroOne(arr, size);
+
+
+
+
+
+
+
+
+    
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if(arr[i]<arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
+        
+    }
+
+    for (int k = 0; k < size; k++)
+    {
+        cout<<arr[k]<<" ";
+    }
     
     
    
