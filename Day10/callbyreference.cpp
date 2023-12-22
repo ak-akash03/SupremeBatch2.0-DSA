391d6dd89589397cf766e0d072ee63bd7cad275c
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-22 13:24:42 +0530

    ...

---
+++ Day10/callbyreference.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int n = 200;
+    // cout<<n<<endl;
+
+    // int &k = n;
+    // k++;
+    // cout<<k<<endl;
+
+    // cout<<n<<endl;
+
+    // int &c = k;
+    // cout<<c<<endl;
+
+
+
+
+
+
+    
+
+}
