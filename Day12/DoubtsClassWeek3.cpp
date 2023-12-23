f00c3a2d2325ef906ac3839a8e0574c0b6ab1b5b
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-23 13:13:46 +0530

    Day12 Commit ..

--- Day12/DoubtsClassWeek3.cpp
+++ Day12/DoubtsClassWeek3.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
+int fun2(int a)
+{
+    cout << a << endl;
+    return a;
+}
+int fun1(int a)
+{
+    int c = fun2(a);
+    return c;
+}
 int main()
 {
+
+    int a = fun1(5);
 
     // float f = 5.0;
     // cout<<f<<endl;
@@ -9,9 +21,6 @@ int main()
     // cout<<ff<<endl;
     // int a = 3.2;
     // cout<<a<<endl;
-
-
-    
 
     return 1;
 }
