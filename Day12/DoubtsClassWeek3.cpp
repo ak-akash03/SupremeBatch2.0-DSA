e9d7cec124029b745a1007d1e4cb0595a915be76
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-23 13:07:48 +0530

    ....

---
+++ Day12/DoubtsClassWeek3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // float f = 5.0;
+    // cout<<f<<endl;
+    // float ff = 5.11;
+    // cout<<ff<<endl;
+    // int a = 3.2;
+    // cout<<a<<endl;
+
+
+    
+
+    return 1;
+}
