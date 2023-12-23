206cecd3b562d6938305f92a24330c686a32750c
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-23 13:21:28 +0530

    ....

--- Day12/DoubtsClassWeek3.cpp
+++ Day12/DoubtsClassWeek3.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
-int fun2(int a)
-{
-    cout << a << endl;
-    return a;
-}
-int fun1(int a)
-{
-    int c = fun2(a);
-    return c;
-}
+// int fun2(int a)
+// {
+//     cout << a << endl;
+//     return a;
+// }
+// int fun1(int a)
+// {
+//     int c = fun2(a);
+//     return c;
+// }
+
+
 int main()
 {
 
-    int a = fun1(5);
+
+
+    
+
+    // int a = fun1(5);
 
     // float f = 5.0;
     // cout<<f<<endl;
